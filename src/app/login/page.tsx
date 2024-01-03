54519b552dd93781970890c7bb3efaa12cb96fc9
karnan7 <adithkayalad@gmail.com>
2024-01-03 23:37:06 +0530

    signup login page added. Database connected

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,63 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+
 export default function LoginPage() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
   return (
-    <div>
-      <h1>Login page</h1>
+    <div className="h-screen w-screen bg-violet-600 flex justify-between">
+      <div className="h-screen flex justify-center flex-1 items-center p-10">
+        <h1 className="text-6xl font-semibold max-w-[350px]">
+          Think Outside The Box.
+        </h1>
+      </div>
+      <div className="text-black bg-white p-[80px] rounded-l-3xl min-w-[700px] flex flex-col items-center">
+        <h1 className="text-4xl font-bold mb-8">Login</h1>
+        <form action="" className="flex flex-col gap-10 w-[450px]">
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            className="border-b-2 outline-none p-2"
+            value={user.email}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            className="border-b-2 outline-none p-2"
+            value={user.password}
+            onChange={handleChange}
+          />
+          <Link
+            href="/forgot"
+            className="no-underline underline-offset-4 hover:underline text-sky-500 text-right"
+          >
+            <span>Forgot Password?</span>
+          </Link>
+          <button className="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded">
+            Login
+          </button>
+          <span>
+            Doesn't have an account ?
+            <Link
+              href="/signup"
+              className="no-underline underline-offset-4 hover:underline text-sky-500"
+            >
+              {" "}
+              signup
+            </Link>
+          </span>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/utils/database.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export async function ConnectToDb() {
+  try {
+    mongoose.connect(process.env.MONGODB_URI!);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log("mongodb connected");
+    });
+
+    connection.on("error", (err) => {
+      console.log("mongodb connection error", err);
+      process.exit();
+    });
+  } catch (error) {
+    console.log("something went wrong !", error);
+  }
+}
