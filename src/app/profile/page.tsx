00337a29701ae3dd7c27853c3222321de137c79b
karnan7 <adithkayalad@gmail.com>
2024-01-09 23:30:01 +0530

    logout feature added.

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
 const ProfilePage = () => {
+  const router = useRouter();
+
+  const handleLogOut = async () => {
+    const response = await axios.get("/api/users/logout");
+    if (response) {
+      router.push("/login");
+    }
+  };
   return (
     <div>
       <h1>profile</h1>
+      <button onClick={handleLogOut}>Logout</button>
     </div>
   );
 };
